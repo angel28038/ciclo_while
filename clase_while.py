bd0b2fd991528fef8ead5f6409dc481342f84512
angel28038 <angeldavidflorez2@gmail.com>
2024-12-04 19:52:44 -0500

    Update clase_while.py

--- clase_while.py
+++ clase_while.py
@@ -24,3 +24,35 @@
 #     suma+= numero
 #     numero= int(input("ingresa un numero o pulsa 0 para salir: "))
 # print (f"la suma total es: {suma}")
+
+# condiciones dinamicas:
+
+# son aleatorias y pueden cambiar con la ejecucion del codigo.
+
+#simulacion basada en una condicion externa
+
+#simular crecimiento de una poblacion hasta que alcance un limite.
+
+# poblacion = 1000
+# limite = 5000
+# tasa_crecimiento = 1.1
+# año=1
+# while poblacion < limite:
+#     print (f" en el año:{año}, la poblacion actual es de: {poblacion}")
+#     poblacion = int(poblacion*tasa_crecimiento)
+#     año+=1
+#     print (f"poblacion final alcanzada: {poblacion}")
+
+#lecturas de un sensor
+# simular las lecturas de un sensor que medira valores aleatorios hasta que alcanze un valor objetivo.
+
+import random
+sensor = random.randint(0,50)
+objetivo=40
+contador=1
+
+while sensor <objetivo:
+    print(f"en la lectura numero: {contador}, el valor del sensor es:{sensor}")
+    sensor+= random.randint(1,10)
+    contador +=1
+print (f"la lectura final alcanzada fue: {sensor}")
